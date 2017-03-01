dd6b47e807f538c9d8aab60616117716bac3ea2b
TheGordoCat <gordos2710@gmail.com>
2017-02-28 22:31:48 -0800

    Update Problem8.java

--- Problem8.java
+++ Problem8.java
@@ -24,25 +24,24 @@ public class Problem8 {
 				maxValue = multiple;
 			begin++;
 		}
-		//System.out.println(numString);
 		System.out.println(maxValue);
 	}
 	
 	
 	
 	public static String readFileAsString(String filePath) throws IOException {
-        StringBuffer fileData = new StringBuffer();
-        BufferedReader reader = new BufferedReader(
-                new FileReader(filePath));
-        char[] buf = new char[1024];
-        int numRead=0;
-        while((numRead=reader.read(buf)) != -1){
-            String readData = String.valueOf(buf, 0, numRead);
-            fileData.append(readData);
-        }
-        reader.close();
-        return fileData.toString();
-    }
+        	StringBuffer fileData = new StringBuffer();
+        	BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        	char[] buf = new char[1024];
+        	int numRead=0;
+		
+       		while((numRead=reader.read(buf)) != -1){
+            		String readData = String.valueOf(buf, 0, numRead);
+            		fileData.append(readData);
+        	}
+        	reader.close();
+        	return fileData.toString();
+    	}
 	
 	
 	public static long multipleOfNums(String num){
