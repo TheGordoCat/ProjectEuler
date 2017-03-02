8522bbb1ac5a79a7ea3990b07d36cb2bcbee13bc
TheGordoCat <gordos2710@gmail.com>
2017-03-01 23:40:01 -0800

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,4 +1,5 @@
 //Sean Gordon, 2/27/17
+//improved on 3/1/17
 //answer is 6857 for the number 600851475143
 //took quite a while to run
 
@@ -10,6 +11,8 @@ public class Problem3 {
 	public static void main(String args[]){
 		
 		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter a number to find the highest prime for: ");
 		final long number = keyboard.nextLong();
 		
 		long maxPrime = largestPrime(number);
@@ -21,34 +24,13 @@ public class Problem3 {
 	//finds the largest prime of a number
 	public static long largestPrime(long number){
 		
+		NumberMethods numberMethods = new NumberMethods();
 		long maxPrime = 1;
+		
 		for (long i = 1; i <= number / 2; i++){
-			if (i % 2 != 0 && i % 5 != 0){			//getting rid of some factors so the runtime isn't awful
-				if ((number % i == 0) && (phi(i) == i - 1) && (i > maxPrime))
-					maxPrime = i;
-			}
+			if ((number % i == 0) && numberMethods.isPrime(i) && (i > maxPrime))
+				maxPrime = i;
 		}
 		return maxPrime;
-	}
-	
-	//finds the amount of numbers coprime to a number
-	//if phi(num) = num - 1, then num is a prime
-	public static long phi(long num){
-		
-		long count = 0;
-		for(long i = 1; i < num; i++){
-			if (hcf(num, i) == 1)
-				count++;
-		}
-		return count;
-	}
-	
-	//finds the highest common factor of two numbers
-	public static long hcf(long integer1, long integer2){
-		
-		if (integer1 % integer2 == 0)
-			return integer2;
-		else
-			return hcf(integer2, integer1 % integer2);
 	}
 }
