778607de9871aff448dac86cfa52aef339b96ef3
TheGordoCat <gordos2710@gmail.com>
2017-03-01 23:18:24 -0800

    Update Problem7.java

--- Problem7.java
+++ Problem7.java
@@ -1,5 +1,6 @@
 //Sean Gordon, 2/27/17
-//the answer is 104743
+//revised on 3/1/17
+//the answer to the 10001th prime is 104743
 
 package ProjectEuler;
 import java.util.Scanner;
@@ -10,41 +11,21 @@ public class Problem7 {
 		
 		Scanner keyboard = new Scanner(System.in);
 		
+		NumberMethods numberMethods = new NumberMethods();
 		int count = 0;
 		long num = 1;
 		int nthPrime = 0;
 		
+		System.out.println("Please enter the nth Prime: ");
 		nthPrime = keyboard.nextInt();
+		
 		while (count != nthPrime){
 			num++;
-			if (phi(num) == num - 1)
+			if (numberMethods.isPrime(num))
 				count++;
 			
 		}
 		System.out.println(num);
 		keyboard.close();
-	}
-	
-	
-	//functions copied from Problem3
-	//finds the amount of numbers coprime to a number
-	//if phi(num) = num - 1, then num is a prime
-	public static long phi(long num){
-				
-		long count = 0;
-		for(long i = 1; i < num; i++){
-			if (hcf(num, i) == 1)
-				count++;
-		}
-		return count;
-	}
-			
-	//finds the highest common factor of two numbers
-	public static long hcf(long integer1, long integer2){
-				
-		if (integer1 % integer2 == 0)
-			return integer2;
-		else
-			return hcf(integer2, integer1 % integer2);
 	}
 }
