6a9901e0bfbb17fcbc87f09bf4f7b6cb3fc303b5
TheGordoCat <gordos2710@gmail.com>
2017-03-01 23:12:41 -0800

    Update Problem10.java

--- Problem10.java
+++ Problem10.java
@@ -7,29 +7,14 @@ public class Problem10 {
 
 	public static void main(String args[]){
 		
+		NumberMethods numberMethods = new NumberMethods();
+		final int MAX_VALUE = 2000000;
 		long sum = 0;
-		for (int i = 1; i < 2000000; i++){
-			if (isPrime(i))
+		
+		for (int i = 1; i < MAX_VALUE; i++){
+			if (numberMethods.isPrime(i))
 				sum += i;
 		}
 		System.out.println(sum);
-	}
-	
-	//found a new way to find primes since the old way took way too long.
-	//I used to find the hcf and determine if phi(i) = i - 1
-	public static boolean isPrime(int num){
-		
-		if (num <= 1)
-			return false;
-		if (num == 2)
-			return true;
-		if (num % 2 == 0)
-			return false;
-		
-		for (int i = 3; i * i <= num; i += 2)
-			if (num % i == 0)
-				return false;
-		
-		return true;
 	}
 }
