bf4cf1d8786205ef29e69c0d421a1c835921fed6
TheGordoCat <gordos2710@gmail.com>
2017-03-21 23:14:38 -0700

    Add files via upload

---
+++ Problems 11-20/Problem13.java
@@ -0,0 +1,31 @@
+//Sean Gordon, 3/15/17 (updated 3/21/17)
+//the answer is 5537376230
+
+package ProjectEuler;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Problem13 {
+
+	public static void main(String args[]) throws IOException{
+	
+			Scanner input = new Scanner(new File("C:/Users/Sean/workspace/ProgrammingByDoing/src/ProjectEuler/problem13.txt"));
+			
+			List<BigInteger> lines = new ArrayList<BigInteger>();
+			while (input.hasNextLine()) {
+				lines.add(new BigInteger(input.nextLine()));
+			}
+			
+			BigInteger sum = BigInteger.ZERO;
+			for (int i = 0; i < lines.size(); i++)
+				sum = sum.add(lines.get(i));
+			
+			System.out.println(sum.toString().substring(0, 10));
+			
+			input.close();
+	}
+}
