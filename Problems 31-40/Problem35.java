8731067ff7a64ecab89e8bb5edaf8ec625789ff4
TheGordoCat <gordos2710@gmail.com>
2017-03-25 18:22:02 -0700

    Add files via upload

---
+++ Problems 31-40/Problem35.java
@@ -0,0 +1,46 @@
+//Sean Gordon, 3/25/17
+//the answer is 55
+//run-time is .44 seconds
+
+package ProjectEuler;
+
+public class Problem35 {
+
+	public static void main(String args[]){
+		
+		long start = System.currentTimeMillis();
+		Problem23();
+		long end = System.currentTimeMillis();
+		System.out.println((double) (end - start) / 1000 + " seconds");
+	}
+	
+	
+	public static void Problem23(){
+		
+		final int LIMIT = 1000000;
+		int count = 0;
+		
+		for (int i = 2; i < LIMIT; i++){
+			if (isCircularPrime(i))
+					count++;
+		}
+		System.out.println(count);
+	}
+	
+	public static boolean isCircularPrime(int prime){
+		
+		NumberMethods numberMethods = new NumberMethods();
+		if (!numberMethods.isPrime(prime))
+			return false;
+		
+		String num = prime + "";
+		for (int i = 0; i < num.length() - 1; i++){
+			char firstNum = num.charAt(0);
+			num = num.substring(1) + firstNum;
+			if (!numberMethods.isPrime(Integer.parseInt(num)))
+				return false;
+		}
+		
+		return true;
+	}
+}
